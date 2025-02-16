626154ddb88cc3648f0758f83b8f235a0fd055d0
catediana <catherinenanyala@gmail.com>
2025-02-16 06:10:11 -0800

    Configured Django Admin for Book model

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+ # Import the Book model
+from .models import Book 
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")  
+    search_fields = ("title", "author")  
+    list_filter = ("publication_year",)  
+
+# Registering the Book model 
+admin.site.register(Book, BookAdmin)
+
+
