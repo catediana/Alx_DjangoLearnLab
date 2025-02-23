78c04488acfcf3132d5e7334343393d2e9415b2b
catediana <catherinenanyala@gmail.com>
2025-02-23 13:19:41 -0800

    modified

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,13 +6,11 @@ from django.http import HttpResponse
 
 # Function-based view that lists all books in the database.
 
+
 def list_books(request):
     books = Book.objects.all()
-    output = "Books Available:\n\n"
-    for book in books:
-        output += f"{book.title} by {book.author.name}\n"
-    return HttpResponse(output, content_type="text/plain")
-
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
