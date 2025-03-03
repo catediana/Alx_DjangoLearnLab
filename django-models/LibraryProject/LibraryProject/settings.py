d40cb58274e494279575fd405187fc36a762fe3d
catediana <catherinenanyala@gmail.com>
2025-03-03 10:59:36 -0800

    modified

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--4myxh78p&tp)13(^=jell)_)%-so*oamif*tc9cj^42!8q!=b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
