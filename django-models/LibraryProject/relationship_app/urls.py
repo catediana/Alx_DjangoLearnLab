789cda9af820e3727c6fda6e8a9b6ff58f49cff5
catediana <catherinenanyala@gmail.com>
2025-03-08 01:49:08 -0800

    modified

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Function-based view for listing all books
@@ -9,5 +10,16 @@ urlpatterns = [
     
     # Class-based view for showing library details
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
+     
+     #login view using django bult in logon view
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    
+    # Logout view using Django's built-in LogoutView with a custom template
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # Registration view using your custom view
+    path('register/', views.register, name='register'),
 
+
+
+ ]
