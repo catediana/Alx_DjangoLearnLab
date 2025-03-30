2b5e9e71e6ac448ed076111d4d828b0d600bce4a
catediana <catherinenanyala@gmail.com>
2025-03-31 01:28:15 +0300

    modified the admin_view.py file

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,13 +1,9 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 
-from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
-
 def is_admin(user):
-    # Check that the user is authenticated, has a UserProfile, and role equals 'Admin'
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
-     return HttpResponse("<h1>Welcome, Admin!</h1><p>This area is for administrative tasks.</p>")
+    return HttpResponse("Welcome, Admin!")
