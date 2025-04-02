d21cafb0fd6042962c251efc7bece120b41a2e73
catediana <catherinenanyala@gmail.com>
2025-04-02 17:31:17 +0300

    modified

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('member_view/', views.member_view, name='member_view'),
 
 #views for secure paths
-    path('add_book/', views.add_book, name='add_book'),  # Change 'add/' to 'add_book/'
-    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # Change 'edit/' to 'edit_book/'
-    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),  # Optional if needed
+    path('add_book/', views.add_book, name='add_book'),  
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),  
 ]
