d7799a2e149f90fd12304ca9b1f17a721a653fe7
catediana <catherinenanyala@gmail.com>
2025-04-04 04:03:49 +0300

    modified forms.py file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,13 +1,7 @@
 from django import forms
-from .models import Book
+from django import forms
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if "<script>" in title:  # Prevent XSS
-            raise forms.ValidationError("Invalid input!")
-        return title
+class ExampleForm(forms.Form):
+    # Define the fields for your form here
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
